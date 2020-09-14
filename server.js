ecf0c7a356f52243173098e99c157bddc8349d14
rurangwa <rurangwajl7@gmail.com>
2020-09-14 14:14:49 +0200

    Push

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('kpc-client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'kpc-client', 'build', 'index.html'));
   });
 }
 
