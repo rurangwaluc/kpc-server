dac489e820be3b83b8748b336ba69defc8d8777f
rurangwa <rurangwajl7@gmail.com>
2020-09-14 14:11:46 +0200

    Push

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use('/uploads', express.static('uploads'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('kpc-client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
